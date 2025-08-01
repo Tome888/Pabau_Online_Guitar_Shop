22da2d24815fb06392a4abf30780137c939cff75
Tome <tomejeftimov8@gmail.com>
2025-08-01 18:12:29 +0200

    finished home page design

--- online_guitar_shop/src/app/[brandId]/page.tsx
+++ online_guitar_shop/src/app/[brandId]/page.tsx
@@ -1,5 +1,3 @@
-// "use client";
-
 "use client";
 
 import { Suspense } from "react";
--- online_guitar_shop/src/app/components/page_components/SelectBrand.tsx
+++ online_guitar_shop/src/app/components/page_components/SelectBrand.tsx
@@ -1,106 +1,35 @@
-// "use client";
-
-// import { gql, useQuery } from "@apollo/client";
-// import { useEffect, useState } from "react";
-// import Image from "next/image";
-// import { useRouter } from "next/navigation";
-
-// const GET_BRANDS = gql`
-//   query {
-//     findAllBrands {
-//       id
-//       name
-//       origin
-//       image
-//     }
-//   }
-// `;
-
-// type Brand = {
-//   id: string;
-//   image: string;
-//   name: string;
-//   origin: string;
-//   __typename: "Brand";
-// };
-
-// export default function SelectBrand() {
-//   const { data, loading, error } = useQuery(GET_BRANDS);
-//   const [brandsArr, setBrandArr] = useState<Brand[] | []>([]);
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     if (data) {
-//       console.log(data);
-//       setBrandArr(data.findAllBrands);
-//     }
-//   }, [data]);
-
-//   if (loading) return <p>Loading...</p>;
-//   if (error) return <p>Error: {error.message}</p>;
-//   return (
-//     <section className=" w-[100%] flex-col justify-center items-center border border-amber-800">
-//       <h2>
-//         Featuring the <span>Best Brands</span>
-//       </h2>
-//       <p>
-//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-//         nihil?
-//       </p>
-
-//       <div className="flex flex-wrap w-full">
-//         {brandsArr &&
-//           brandsArr.map((brand) => (
-//             <div
-//               className="relative w-[20%] h-[100px] cursor-pointer transform transition-transform duration-300 hover:scale-105"
-//               key={brand.id}
-//               onClick={() => router.push(`/${brand.id}`)}
-//             >
-//               <Image
-//                 src={brand.image}
-//                 alt={brand.name}
-//                 fill
-//                 className="object-contain h-auto w-[150px] filter grayscale opacity-55"
-//                 sizes="(max-width: 768px) 100vw, 150px"
-//                 priority={true}
-//               />
-//             </div>
-//           ))}
-//       </div>
-//     </section>
-//   );
-// }
-
-// components/SelectBrand.tsx
 "use client";
 
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { useBrandContext } from "@/app/providers/BrandContext";
+import { useLanguage } from "@/app/providers/LanguageContext";
+import home from "../../../../translations/home";
 
 export default function SelectBrand() {
   const router = useRouter();
   const { brands, loading, error } = useBrandContext();
+  const { language } = useLanguage();
+  const t = home[language];
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <div className="spinner"></div>;
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <section className="w-full flex-col justify-center items-center border border-amber-800">
-      <h2>
-        Featuring the <span>Best Brands</span>
+    <section className="w-full flex flex-col justify-center items-center gap-1 pt-25  pb-25">
+      <h2 className="text-[44px] font-bold text-zinc-950 text-center">
+        {t.brandSecTitle}
       </h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        nihil?
+      <p className="text-lg text-[rgba(102, 102, 102, 1)] text-center mb-15">
+        {t.smallBrandSecTitle}
       </p>
 
-      <div className="flex flex-wrap w-full">
+      <div className="flex flex-wrap items-center justify-center w-full gap-3">
         {brands.map((brand) => (
           <div
             key={brand.id}
             onClick={() => router.push(`/${brand.id}`)}
-            className="relative w-[20%] h-[100px] cursor-pointer transform transition-transform duration-300 hover:scale-105"
+            className="relative w-[18%] h-[100px] cursor-pointer transform transition-transform duration-300 hover:scale-105"
           >
             <Image
               src={brand.image}
